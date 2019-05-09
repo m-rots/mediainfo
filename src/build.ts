e202694afd0180076eebc8c0a62a7aaf16da8330
m-rots <itsstormyt@gmail.com>
2019-05-09 21:03:51 +0200

    Initial Commit :rocket:

---
+++ src/build.ts
@@ -0,0 +1,43 @@
+import { createWriteStream } from 'fs';
+
+const MediaInfoLib = require('../lib/MediaInfoWasm.js');
+
+let MediaInfoModule: any = false;
+
+async function Init() {
+  return new Promise((resolve) => {
+    MediaInfoModule = MediaInfoLib({
+      postRun: () => {
+        resolve();
+      },
+    });
+  });
+}
+
+async function main() {
+  if (!MediaInfoModule) {
+    await Init();
+  }
+  const file = createWriteStream('./src/types.ts');
+
+  const parameters: string[] = MediaInfoModule.MediaInfo.Option_Static('Info_Parameters').split(/\r?\n/);
+  const lines = parameters.map(line => line.split(':')[0].trim().replace(/[()]/g, ''));
+
+  while (lines.length) {
+    const index = lines.findIndex(line => line === '');
+    const category = lines.splice(0, (index === -1) ? lines.length : index + 1);
+    const type = category.shift();
+    const filtered = Array.from(new Set(category.filter(line => !line.match(/[/\-*]/g) && line.length > 0)));
+
+    file.write(`export type ${type} = {\n`);
+    file.write(`  '@type': '${type}'\n`);
+    filtered.forEach((line) => {
+      file.write(`  ${line}: string\n`);
+    });
+    file.write(`}\n${lines.length ? '\n' : ''}`);
+  }
+
+  file.end();
+}
+
+main();
---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import { PathLike, createReadStream, statSync } from 'fs';
+import {
+  Audio, General, Image, Menu, Other, Text, Video,
+} from './types';
+
+const MediaInfoLib = require('../lib/MediaInfoWasm.js');
+
+interface MediaInfoResponse {
+  media: {
+    '@ref': string
+    track: [
+      Audio | General | Image | Menu | Other | Text | Video
+    ]
+  }
+}
+
+let MediaInfoModule: any = false;
+
+async function Init() {
+  return new Promise((resolve) => {
+    MediaInfoModule = MediaInfoLib({
+      postRun: () => {
+        resolve();
+      },
+    });
+  });
+}
+
+export default async function MediaInfo(path: PathLike): Promise<MediaInfoResponse> {
+  if (!MediaInfoModule) {
+    await Init();
+  }
+  return new Promise((resolve) => {
+    const stream = createReadStream(path, {
+      highWaterMark: 1024 * 1024,
+    });
+    const { size } = statSync(path);
+
+    const MI = new MediaInfoModule.MediaInfo();
+    MI.Open_Buffer_Init(size, 0);
+
+    let seekTo: number;
+
+    stream.on('data', (chunk) => {
+      MI.Open_Buffer_Continue(chunk);
+      seekTo = MI.Open_Buffer_Continue_Goto_Get();
+      // console.log('SeekTo', seekTo);
+
+      if (seekTo !== -1) {
+        MI.Open_Buffer_Init(size, seekTo);
+        stream.close();
+      }
+    });
+
+    stream.on('close', () => {
+      const newstream = createReadStream(path, {
+        highWaterMark: 1024 * 1024,
+        start: seekTo,
+      });
+
+      newstream.on('data', (chunk) => {
+        MI.Open_Buffer_Continue(chunk);
+        seekTo = MI.Open_Buffer_Continue_Goto_Get();
+        // console.log('SeekTo', seekTo);
+      });
+
+      newstream.on('close', () => {
+        MI.Open_Buffer_Finalize();
+
+        MI.Option('Output', 'JSON');
+        MI.Option('Complete');
+        const output: MediaInfoResponse = JSON.parse(MI.Inform());
+        MI.Close();
+        MI.delete();
+
+        resolve(output);
+      });
+    });
+  });
+}
